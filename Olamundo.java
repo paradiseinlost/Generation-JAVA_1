388741b9869eb7dad74bd9e56cb0f5a9a297c16c
Thais <scardoso.thais@gmail.com>
2023-02-07 17:36:51 -0300

    Adicionando exercicios de JAVA

---
+++ Olamundo.java
@@ -0,0 +1,26 @@
+package Aulajava;
+
+import java.util.Scanner;
+
+public class Olamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String nome=" thiago ferreira";
+    int idade=31;
+    double altura= 1.70,nota1,nota2,nota3,media;
+    
+   Scanner leia = new Scanner(System.in);
+    
+    System.out.println ("\nSeu nome é:"+nome);
+    System.out.println ("\nSuaidade é:"+idade);
+    System.out.println ("\nSua altura é:"+altura);
+    
+    System.out.println("\nEntre com sua primeira nota");	
+    nota1 = leia.nextdouble ();
+    		
+    		
+    
+	}
+
+}
